77cbccd10cecad5d276bb8422578d1ebc85986b8
Aslam Ansari <aansari@quark.com>
2025-07-09 19:38:07 +0530

    used lambork feature

--- src/main/java/com/aslam/expense_tracker/dto/ExpenseRequestDTO.java
+++ src/main/java/com/aslam/expense_tracker/dto/ExpenseRequestDTO.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class ExpenseRequestDTO {
     @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", message = "Amount must be greater than 0.01")
@@ -21,35 +23,4 @@ public class ExpenseRequestDTO {
     @Size(max = 100, message = "Description can't exceed 100 characters")
     private String description;
 
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/com/aslam/expense_tracker/model/Expense.java
+++ src/main/java/com/aslam/expense_tracker/model/Expense.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,66 +24,4 @@ public class Expense {
     private LocalDate date;
     private String description;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    // ✅ No-arg constructor (REQUIRED by JPA)
-    public Expense() {
-    }
-
-    public Expense(double amount, String category, LocalDate date, String description, long id) {
-        this.amount = amount;
-        this.category = category;
-        this.date = date;
-        this.description = description;
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Expense{" +
-                "amount=" + amount +
-                ", id=" + id +
-                ", category='" + category + '\'' +
-                ", date=" + date +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
